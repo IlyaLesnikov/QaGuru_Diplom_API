4331a7313cf83ef70613c0ae7e3900fa7c845a02
i.lesnikov <i.lesnikov@bars-group.ru>
2024-02-22 21:52:48 +0300

    Added allure steps

--- src/test/java/guru/qa/tests/ApiTests.java
+++ src/test/java/guru/qa/tests/ApiTests.java
@@ -3,22 +3,20 @@ package guru.qa.tests;
 import guru.qa.endpoints.EndPoints;
 import guru.qa.models.*;
 import guru.qa.requests.BookingRequests;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import java.util.Collections;
-
+import static guru.qa.helpres.CustomAllureListener.withCustomTemplates;
 import static guru.qa.specifications.Spec.*;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.IsEqual.equalTo;
+
 
 public class ApiTests {
     EndPoints endPoints = new EndPoints();
     BookingRequests bookingRequests = new BookingRequests();
+
     @Test
     @Tag("API")
     @DisplayName("Создание книги")
@@ -26,15 +24,17 @@ public class ApiTests {
         CreateBookingDatesRequest createBookingDates = new CreateBookingDatesRequest("2020-01-01", "2023-01-01");
         CreateBookingRequest createBooking = new CreateBookingRequest("Jim", "Brown", 111, true, createBookingDates, "Breakfast");
 
-        CreateBookingResponse createBookingResponse = given()
+        CreateBookingResponse createBookingResponse = step("Отправка запроса на БЭК", () ->
+                given()
                 .spec(requestSpec)
                 .body(createBooking)
                 .when()
                 .post(endPoints.getBOOK_PATH())
                 .then()
                 .spec(responseSpec200OK)
-                .extract().as(CreateBookingResponse.class);
+                .extract().as(CreateBookingResponse.class));
 
+        step("Проверка тело ответа от БЭКа");
         Assertions.assertEquals(createBooking.getFirstname(), createBookingResponse.getBooking().getFirstname());
         Assertions.assertEquals(createBooking.getLastname(), createBookingResponse.getBooking().getLastname());
         Assertions.assertEquals(createBooking.getTotalprice(), createBookingResponse.getBooking().getTotalprice());
@@ -48,22 +48,24 @@ public class ApiTests {
     @Tag("API")
     @DisplayName("Получение определенной книги")
     protected void gettingACertainBookTest() {
-        GetBookingResponse getBookingRequest = given()
+        GetBookingResponse getBookingRequest = step("Отправка запроса на БЭК", () -> given()
                 .spec(requestSpec)
                 .when()
                 .get(endPoints.getBOOK_PATH() + "/" + bookingRequests.getBookId())
                 .then()
                 .log().body()
                 .spec(responseSpec200OK)
-                .extract().as(GetBookingResponse.class);
+                .extract().as(GetBookingResponse.class));
 
-        Assertions.assertEquals("Ilya", getBookingRequest.getFirstname());
-        Assertions.assertEquals("Lesnikov", getBookingRequest.getLastname());
-        Assertions.assertEquals(1550, getBookingRequest.getTotalprice());
-        Assertions.assertTrue(getBookingRequest.getDepositpaid());
-        Assertions.assertEquals("2020-02-23", getBookingRequest.getBookingdates().getCheckin());
-        Assertions.assertEquals("2024-02-23", getBookingRequest.getBookingdates().getCheckout());
-        Assertions.assertEquals("QA", getBookingRequest.getAdditionalneeds());
+        step("Проверка тело ответа от БЭКа", () -> {
+            Assertions.assertEquals("Ilya", getBookingRequest.getFirstname());
+            Assertions.assertEquals("Lesnikov", getBookingRequest.getLastname());
+            Assertions.assertEquals(1550, getBookingRequest.getTotalprice());
+            Assertions.assertTrue(getBookingRequest.getDepositpaid());
+            Assertions.assertEquals("2020-02-23", getBookingRequest.getBookingdates().getCheckin());
+            Assertions.assertEquals("2024-02-23", getBookingRequest.getBookingdates().getCheckout());
+            Assertions.assertEquals("QA", getBookingRequest.getAdditionalneeds());
+        });
     }
 
     @Test
@@ -73,7 +75,8 @@ public class ApiTests {
         CreateBookingDatesRequest createBookingDates = new CreateBookingDatesRequest(null, null);
         CreateBookingRequest updateBooking = new CreateBookingRequest("Jim", "Brown", null, null, null, null);
 
-        GetBookingResponse updateBookingResponse = given()
+        GetBookingResponse updateBookingResponse = step("Отправка запроса на БЭК", () ->
+                given()
                 .spec(requestSpec)
                 .body(updateBooking)
                 .header("Cookie", "token=" + bookingRequests.getCookie())
@@ -81,10 +84,12 @@ public class ApiTests {
                 .patch(endPoints.getBOOK_PATH() + "/" + bookingRequests.getBookId())
                 .then()
                 .spec(responseSpec200OK)
-                .extract().as(GetBookingResponse.class);
+                .extract().as(GetBookingResponse.class));
 
-        Assertions.assertEquals(updateBooking.getFirstname(), updateBookingResponse.getFirstname());
-        Assertions.assertEquals(updateBooking.getLastname(), updateBookingResponse.getLastname());
+        step("Проверка тело ответа от БЭКа", () -> {
+            Assertions.assertEquals(updateBooking.getFirstname(), updateBookingResponse.getFirstname());
+            Assertions.assertEquals(updateBooking.getLastname(), updateBookingResponse.getLastname());
+        });
     }
 
     @Test
@@ -94,24 +99,30 @@ public class ApiTests {
         CreateBookingDatesRequest createBookingDates = new CreateBookingDatesRequest("2019-01-01", "2020-01-01");
         CreateBookingRequest updateBooking = new CreateBookingRequest("Jim", "Brown", 10000, false, createBookingDates, "QC");
 
-        GetBookingResponse updateBookingResponse = given()
-                .spec(requestSpec)
-                .body(updateBooking)
-                .header("Cookie", "token=" + bookingRequests.getCookie())
-                .when()
-                .put(endPoints.getBOOK_PATH() + "/" + bookingRequests.getBookId())
-                .then()
-                .spec(responseSpec200OK)
-                .extract().as(GetBookingResponse.class);
+        GetBookingResponse updateBookingResponse = step("Отправка запроса на БЭК", () ->
+                    given()
+                    .filter(withCustomTemplates())
+                    .spec(requestSpec)
+                    .body(updateBooking)
+                    .header("Cookie", "token=" + bookingRequests.getCookie())
+                    .when()
+                    .put(endPoints.getBOOK_PATH() + "/" + bookingRequests.getBookId())
+                    .then()
+                    .spec(responseSpec200OK)
+                    .extract().as(GetBookingResponse.class));
 
-        Assertions.assertEquals(updateBooking.getFirstname(), updateBookingResponse.getFirstname());
-        Assertions.assertEquals(updateBooking.getLastname(), updateBookingResponse.getLastname());
-        Assertions.assertEquals(updateBooking.getTotalprice(), updateBookingResponse.getTotalprice());
-        Assertions.assertFalse(updateBookingResponse.getDepositpaid());
-        Assertions.assertEquals(createBookingDates.getCheckin(), updateBookingResponse.getBookingdates().getCheckin());
-        Assertions.assertEquals(createBookingDates.getCheckout(), updateBookingResponse.getBookingdates().getCheckout());
-        Assertions.assertEquals(updateBooking.getAdditionalneeds(), updateBookingResponse.getAdditionalneeds());
+        step("Проверка тело ответа от БЭКа", () -> {
+            Assertions.assertEquals(updateBooking.getFirstname(), updateBookingResponse.getFirstname());
+            Assertions.assertEquals(updateBooking.getLastname(), updateBookingResponse.getLastname());
+            Assertions.assertEquals(updateBooking.getTotalprice(), updateBookingResponse.getTotalprice());
+            Assertions.assertFalse(updateBookingResponse.getDepositpaid());
+            Assertions.assertEquals(createBookingDates.getCheckin(), updateBookingResponse.getBookingdates().getCheckin());
+            Assertions.assertEquals(createBookingDates.getCheckout(), updateBookingResponse.getBookingdates().getCheckout());
+            Assertions.assertEquals(updateBooking.getAdditionalneeds(), updateBookingResponse.getAdditionalneeds());
+        });
     }
+}
+
 //    @Test
 //    @Tag("API")
 //    @DisplayName("Обновление информации о книге")
@@ -124,4 +135,3 @@ public class ApiTests {
 //                .then()
 //                .body("", Matchers.hasSize(0));
 //    }
-}
